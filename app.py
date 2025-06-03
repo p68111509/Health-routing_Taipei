e3b3a919bd44c1f06e4ab691d7a6db7dd859bc9d
p68111509 <p68111509@gs.ncku.edu.tw>
2025-06-03 22:26:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -748,7 +748,7 @@ st.markdown(f"""
 
     <div style="text-align: center; font-size: 13px; color: #666; font-family: 'Noto Sans TC', 'Microsoft JhengHei', sans-serif;">
         <p style="margin-bottom: 4px;">
-            © 2025 許家瑋 林祐如｜國立成功大學 測量及空間資訊學系｜指導老師：吳治達 教授<br>
+            © 2025 許家瑋 林侑萱 楊蜹畛｜國立成功大學 測量及空間資訊學系<br>
             聯絡信箱：<a href="mailto:p68111509@gs.ncku.edu.tw">p68111509@gs.ncku.edu.tw</a>｜GitHub 專案：<a href="https://github.com/p68111509/Health-routing_Taichung" target="_blank">Health-routing_Taichung</a>
         </p>
         <p style="margin-top: 6px; margin-bottom: 10px;">
